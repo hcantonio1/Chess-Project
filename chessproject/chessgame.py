a533d34257a984e62b97d6746f8b45820d95fd2c
hcantonio1 <63497385+hcantonio1@users.noreply.github.com>
2020-08-22 17:05:15 +0800

    En Passant Implemented
    Captured pieces now removed from the board
    Black and white now take turns
    En passant implemented

--- chessproject/chessgame.py
+++ chessproject/chessgame.py
@@ -5,18 +5,18 @@ import board_and_pieces as bp
 
 class ChessGame:
     initial_position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
-    def __init__(self, position_str, current_turn, castling_posibilities, en_passant_coordinates):
+    def __init__(self, position_str, current_turn, castling_posibilities, en_passant_squares):
         self.board = bp.Board()
         self.pieces = self.get_pieces(position_str)
         self.current_turn = current_turn
         self.castling_posibilities = castling_posibilities
-        self.en_passant_coordinates = en_passant_coordinates
+        self.en_passant_squares = en_passant_squares
 
         self.batch = pyglet.graphics.Batch()
         for piece in self.pieces:
             piece.sprite.batch = self.batch
 
-        self.a_piece_is_selected = False
+
         self.selected_piece = None
 
     def __str__(self):
@@ -73,23 +73,31 @@ class ChessGame:
         file, rank = self.board.file_rank_of_xy(x, y)
         if self.is_out_of_bounds_fr(file, rank):
             pass
-        elif self.a_piece_is_selected:
+        elif self.selected_piece is not None:
             self.selected_piece.on_click(x, y)
-            self.a_piece_is_selected = False
             self.selected_piece = None
         else:
-            for piece in self.pieces:
-                if piece.is_on(x, y):
-                    self.selected_piece = piece
-                    self.a_piece_is_selected = True
-                    print(str(piece) + " is selected")
-                    piece.on_click(x, y)
-                    break
+            sq = self.board.square_of_xy(x, y)
+            piece = self.piece_at_square(sq)
+            if not piece.is_my_turn():
+                return False
+            print(str(piece) + " is selected")
+            self.selected_piece = piece
+            piece.on_click(x, y)
+
 
     def piece_at_square(self, square):
         # can be O(1) with 64 square objects
         for piece in self.pieces:
-            #print(square, piece.square)
             if square == piece.square:
                 return piece
         return None
+        
+    def finish_turn(self):
+        self.current_turn += 1
+
+    def remove_piece_at_sq(self, square):
+        self.pieces.remove(self.piece_at_square(square))
+
+    def remove_piece(self, piece):
+        self.pieces.remove(piece)
